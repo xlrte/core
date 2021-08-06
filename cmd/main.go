2025ab905b1ae4437633f477996a398be7b7ca24
Wille Faler <wille.faler@gmail.com>
2021-08-06 16:50:58 +0200

    use tf programmatically

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,6 +11,9 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/xlrte/core/pkg/api"
 	"gopkg.in/yaml.v2"
+
+	"github.com/hashicorp/terraform-exec/tfexec"
+	"github.com/hashicorp/terraform-exec/tfinstall"
 )
 
 // a Runtime has a Driver, that supports potentially resources and network stanzas?
@@ -17,6 +21,57 @@ import (
 // As in static resource defined per env? or have some way of mixing in other resources?
 
 func main() {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(path)
+
+	// tmpDir, err := ioutil.TempDir("", "tfinstall")
+	// if err != nil {
+	// 	log.Fatalf("error creating temp dir: %s", err)
+	// }
+	// defer os.RemoveAll(tmpDir)
+
+	execPath, err := tfinstall.Find(context.Background(), &tfinstall.LookPathOption{}) //tfinstall.LatestVersion(tmpDir, false))
+	if err != nil {
+		log.Fatalf("error locating Terraform binary: %s", err)
+	}
+	fmt.Println("found Terraform")
+
+	workingDir := "deployment"
+	tf, err := tfexec.NewTerraform(workingDir, execPath)
+	if err != nil {
+		log.Fatalf("error running NewTerraform: %s", err)
+	}
+
+	fmt.Println("Terraform started")
+
+	err = tf.Init(context.Background(), tfexec.Upgrade(true))
+	if err != nil {
+		log.Fatalf("error running Init: %s", err)
+	}
+
+	fmt.Println("Terraform init")
+
+	state, err := tf.Show(context.Background())
+	if err != nil {
+		log.Fatalf("error running Show: %s", err)
+	}
+
+	fmt.Println("Terraform state read")
+	boo, err := tf.Plan(context.Background())
+	fmt.Println(boo)
+
+	fmt.Println("Terraform planned")
+
+	fmt.Println(*state.Values.Outputs["cloud-run-url"])
+
+	fmt.Println(state.FormatVersion) // "0.1"
+
+}
+
+func readYa() {
 	var service api.Service
 	var http api.Http
 
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/go-playground/validator/v10 v10.8.0
+	github.com/hashicorp/terraform-exec v0.14.0
 	gopkg.in/yaml.v2 v2.4.0
 )
